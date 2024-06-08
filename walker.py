51034fdfb8b1d5619860ff42f13cb3879a9ea53f
neil20030304 <75361519+neil20030304@users.noreply.github.com>
2024-06-08 15:00:12 -0400

    updated walker

--- walker.py
+++ walker.py
@@ -1,83 +1,40 @@
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-import keyboard
-import threading
-import time
+from time import sleep
 
-class Walker(Node):
+class TwistPublisher(Node):
     def __init__(self):
-        super().__init__('walker')
+        super().__init__('twist_publisher')
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.linear_speed = 0.0
-        self.angular_speed = 0.0
-        self.moving = False
+        self.timer_period = 0.1  # seconds
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        self.start_time = self.get_clock().now().seconds_nanoseconds()[0]
+        self.is_moving = True
 
-        # Start the keyboard listener in a separate thread
-        self.keyboard_thread = threading.Thread(target=self.keyboard_listener)
-        self.keyboard_thread.daemon = True
-        self.keyboard_thread.start()
+    def timer_callback(self):
+        current_time = self.get_clock().now().seconds_nanoseconds()[0]
+        elapsed_time = current_time - self.start_time
 
-    def keyboard_listener(self):
-        while True:
-            if keyboard.is_pressed('w'):
-                self.linear_speed = 1.0
-                self.angular_speed = 0.0
-                self.moving = True
-            # elif keyboard.is_pressed('s'):
-            #     self.linear_speed = -1.0
-            #     self.angular_speed = 0.0
-            #     self.moving = True
-            # elif keyboard.is_pressed('a'):
-            #     self.linear_speed = 0.0
-            #     self.angular_speed = 1.0
-            #     self.moving = True
-            # elif keyboard.is_pressed('d'):
-            #     self.linear_speed = 0.0
-            #     self.angular_speed = -1.0
-            #     self.moving = True
-            # elif keyboard.is_pressed('q'):
-            #     self.linear_speed = 0.5
-            #     self.angular_speed = 0.5
-            #     self.moving = True
-            # elif keyboard.is_pressed('e'):
-            #     self.linear_speed = 0.5
-            #     self.angular_speed = -0.5
-            #     self.moving = True
-            # else:
-            #     self.moving = False
-            
-            # if self.moving:
-            #     self.move_for_duration(5)
-            #     self.moving = False
-            
-            time.sleep(0.1)
+        twist = Twist()
+        if elapsed_time < 2.0:
+            twist.linear.x = 0.5
+        else:
+            twist.linear.x = 0.0
+            self.is_moving = False
 
-    def move_for_duration(self, duration):
-        start_time = time.time()
-        while time.time() - start_time < duration:
-            msg = Twist()
-            msg.linear.x = self.linear_speed
-            msg.angular.z = self.angular_speed
-            self.publisher_.publish(msg)
-            time.sleep(0.1)
+        self.publisher_.publish(twist)
 
-        # Stop the robot after moving
-        msg = Twist()
-        msg.linear.x = 0.0
-        msg.angular.z = 0.0
-        self.publisher_.publish(msg)
+        if not self.is_moving:
+            self.get_logger().info('Stopping the robot.')
+            self.timer.cancel()
+            rclpy.shutdown()
 
 def main(args=None):
     rclpy.init(args=args)
-    walker = Walker()
-    try:
-        rclpy.spin(walker)
-    except KeyboardInterrupt:
-        pass
-
-    walker.destroy_node()
+    twist_publisher = TwistPublisher()
+    rclpy.spin(twist_publisher)
+    twist_publisher.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
